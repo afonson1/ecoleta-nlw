9275d9d529f14df87191abe4001ff1c05e44c034
Afonso Serafim Jacinto <aphonson1@gmail.com>
2020-06-04 21:24:30 -0300

    Configuração do Knexfile
    o "knexfile.ts" é o responsável poralgumas configurações que não estão presentes no "connection", como os migrations e os seeds

---
+++ server/knexfile.ts
@@ -0,0 +1,19 @@
+// Vai ter algumas configurações que a nossa conexão com o banco não tem
+
+// Para poder utilizar os caminhos dos diretórios
+import path from 'path';
+
+// O knex ainda não suporta a sintaxe 'export default', mesmo sendo um arquivo typescript
+module.exports = {
+    // De acordo com a documentação do knex precisamos instalar o SQLite, para isso 'npm install sqlite3'.
+    client: "sqlite3",
+    connection: {
+        filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite'), /*
+        - 'resolve' padroniza o caminho, pois dependendo do sistema operacional, podemos ter \ ou /, i.e.. 
+        - O "__dirname" retorna o caminho para o diretório do arquivo que está executando ele.
+        - 'database.sqlite' é o nome do nosso banco gerado.*/
+    },
+    migrations: {
+        diretório: path.resolve(__dirname, 'src', 'database', 'migrations')
+    }
+};
