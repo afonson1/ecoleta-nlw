164ab45cc24a3a8ad3a2fc6203bb1fa76f59a33a
Afonso Serafim Jacinto <aphonson1@gmail.com>
2020-06-04 23:17:30 -0300

    Adicionando os itens
    Seeds

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,14 +2,18 @@
 // Arquivo principal
 
 import express from 'express'; // Biblioteca Express - microframework para lidar com rotas (url)
-import routes from './routes' // Importando o arquivo criado para manter todas as nossas rotas organizadas
-// o "./" é usado pois estamos acessando um arquivo da nossa aplicação, neste caso na mesma pasta.
+import routes from './routes' /* Importando o arquivo criado para manter todas as nossas rotas organizadas. 
+                                O "./" é usado pois estamos acessando um arquivo da nossa aplicação, neste caso na mesma pasta.*/
+import path from 'path';
 
 const app = express(); //Criando minha aplicação
 
 
-app.use(express.json()); // O express só entende o inglês normal, para que entenda Arquivos Json é necessário esse comando.
+app.use(express.json()); // O express só entende o inglês, para que entenda Json é necessário esse comando.
+app.use(routes); // temos um arquivo 'routes.ts'
 
-app.use(routes); //
+// Função para servir arquivos estáticos: imagens, pdf, arquivos que queremos deixar como download, etc.
+// Podemos acessar pelo 'localhost:porta/uploads/nome_do_arquivo'. Ex:. localhost:3333/uploads/baterias.svg
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333); //Porta que queremos executar a aplicação, pode escolher a porta que preferir
